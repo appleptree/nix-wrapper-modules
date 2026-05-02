3a6a93050910bb4a231a5a659fbf3317e0e947d2
Pancake Appletree <189892522+appleptree@users.noreply.github.com>
2026-05-02 23:24:49 +0200

    feat(wrapperModules.bat): init

---
+++ wrapperModules/b/bat/module.nix
@@ -0,0 +1,49 @@
+{
+  config,
+  wlib,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  imports = [ wlib.modules.default ];
+
+  options = {
+    configFile = lib.mkOption {
+      type = wlib.types.file pkgs;
+      default.content = "";
+      description = ''
+        Bat flags to include via config file
+      '';
+    };
+    themes = lib.mkOption {
+      type = lib.types.attrsOf (wlib.types.file pkgs);
+      default = { };
+      description = ''
+        Bat themes to link to theme directory
+      '';
+    };
+  };
+
+  config =
+    let
+      themes-constructFiles = lib.concatMapAttrs (name: value: {
+        "themes-${name}" = {
+          content = builtins.readFile value.path;
+          relPath = "themes/${name}";
+        };
+      }) config.themes;
+    in
+    {
+      package = lib.mkDefault pkgs.bat;
+      env.BAT_CONFIG_DIR = "${placeholder "out"}/themes";
+      constructFiles = {
+        generatedConfig = {
+          content = builtins.readFile config.configFile.path;
+          relPath = "${config.binName}-config/config";
+        };
+      }
+      // themes-constructFiles;
+      meta.maintainers = [ wlib.maintainers.appleptree ];
+    };
+}
